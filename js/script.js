19ddd41afcca9ccd1b81a3affd74ef78d0a26e91
Zakharchyn Yurii <78864022+ZakharchynYurii@users.noreply.github.com>
2021-03-02 20:40:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Sticky menu background
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 150){
+        document.querySelector('#navbar').style.opacity = 0.8;
+    }else{
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 60
+            },
+            800
+        );
+    }
+});
+
+// Map script
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 49.720886, lng: 23.909216 },
+        zoom: 12,
+    });
+}
